c725a2741257521b4d1bf0341c3471f2d010bfb6
Zachary Picco <zacharypicco@gmail.com>
2025-09-17 13:08:54 -0500

    Fix crash on load.

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,7 +1,2 @@
-require "dependency-graph-lib.data-final-fixes"
-
-local autotech_class = require "autotech"
-
 local config = table.deepcopy(data.raw["mod-data"]["autotech-config"].data)
-local autotech = autotech_class.create(config)
-autotech:run()
+require("autotech").create(config):run()
